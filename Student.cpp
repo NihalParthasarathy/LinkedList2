67bbb4ebb1cc316507d5f97af785dd3b64f6a0bd
Bobbert Banana <hanuman.bheema@gmail.com>
2022-01-22 22:10:43 +0000

    Done with add and print

--- Student.cpp
+++ Student.cpp
@@ -12,5 +12,5 @@ void Student::setStudent(char* newfirst, char* newlast, int newid, float newgpa)
 }
 
 void Student::printStudent() {//Prints out what the student has
-  cout << "First Name:" << first << " Last Name:" << last << " ID:" << id << " GPA:"<< gpa;
+  cout << "First Name:" << first << " Last Name:" << last << " ID:" << id << " GPA:"<< gpa << endl;
 }
--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ void add(Student* s, Node* &head, Node* prev, Node* curr, int compareID);
 void print(Node* next);
 void deleteStudent();
 void avarage();
+void printIT(Node* next);
 
 int main() {
   Node* head = NULL;
@@ -22,8 +23,9 @@ int main() {
     int id;
     float gpa;
     cout << "Would you like to ADD, PRINT, DELETE, AVARAGE, or QUIT" << endl;
+    cin >> input;
     if (strcmp(input, "ADD") == 0) {//Calls the add function
-      Student* temp;
+      Student* temp = new Student();
       cout << "Enter first name" << endl;
       cin >> temp->first;
       cout << "Enter last name" << endl;
@@ -32,15 +34,17 @@ int main() {
       cin >> temp->id;
       cout << "Enter GPA" << endl;
       cin >> temp->gpa;
-      temp->setStudent(first, last, id, gpa);
+      //temp->setStudent(first, last, id, gpa);
+      //temp->printStudent();
       int value = temp->id;
-      add(temp, head, value);
+      add(temp, head, head, head, value);
     }
     else if (strcmp(input, "PRINT") == 0) {//Calls the print function
-      print();
+      print(head);
+      //printIT(head);
     }
     else if (strcmp(input, "DELETE") == 0) {//Calls the delete function
-      delete();
+      //delete();
     }
     else if (strcmp(input, "AVARAGE") == 0) {
 
@@ -55,21 +59,41 @@ int main() {
 void add(Student* s, Node* &head, Node* prev, Node* curr, int compareID) {
   if (head == NULL) {
     head = new Node(s);
+    head->setNext(NULL);
   }
   else if (curr == NULL) {
-    prev->setNext() = new Node(s);
+    cout << "daso" << endl;
+    prev->setNext(new Node(s));
+    prev->getNext()->setNext(NULL);
+  }
+  else if (compareID < head->getStudent()->id) {
+    Node* temp = head;
+    head = new Node(s);
+    head->setNext(temp);// = temp;
   }
   else if (compareID < curr->getStudent()->id) {
-    prev->setNext() = new Node(s);
-    prev->setNext()->setNext() = curr;
+    prev->setNext(new Node(s)); //= new Node(s);
+    prev->getNext()->setNext(curr);// = curr;
+  }
+  else {
+    cout << "Going again" << endl;
+    add(s, head, curr, curr->getNext(), compareID);
   }
 }
 
-void print(Node* next) {
-  if (next == head) {
-    cout << "List:" << endl;
+void printIT(Node* next) {
+  Node* current = next;
+  while (current != NULL) {
+    current->getStudent()->printStudent();
+    current = current->getNext();
   }
+}
+void print(Node* next) {
+  /*if (next == head) {
+    cout << "List:" << endl;
+  }*/
   if (next != NULL) {
+    //next->printStudent();
     next->getStudent()->printStudent();
     print(next->getNext());
   }
